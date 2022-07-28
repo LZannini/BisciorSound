87ee88289e97290118551f683fb5bb1cd802fc3b
Loris_Zannini <97295609+LZannini@users.noreply.github.com>
2022-07-28 16:09:53 +0200

    Add files via upload

---
+++ backup oo/Progetto_OO-BD/src/FunzioniPG_DAO/listaTracce.java
@@ -0,0 +1,65 @@
+package FunzioniPG_DAO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+
+public class listaTracce {
+	
+	public static JList mostra_tracce(JList lista) {
+		
+		String query = "select nome from traccia order by nome";
+		DefaultListModel model = new DefaultListModel();  
+
+	    Statement st = null;
+	    ResultSet rs = null;
+	    Connection conn = null;
+		
+		
+		try {
+			conn = ConfigurazioneDB.ConnessioneDB.connetti();
+			st = conn.createStatement();
+			rs = st.executeQuery(query);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	    try {
+			while (rs.next())
+			{
+			    String itemCodeNome = null;
+				try {
+					itemCodeNome = rs.getString("nome");
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}  
+			    model.addElement(itemCodeNome);  
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    lista.setModel(model);
+
+	    try {
+			rs.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    try {
+			st.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return lista;
+	}
+
+}
